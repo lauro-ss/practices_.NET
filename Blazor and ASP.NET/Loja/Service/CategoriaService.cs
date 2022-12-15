262aca81d5f61a1c2347ed8e7733eb1c67fa96db
Lauro Santana Silva <bestlaurobr@gmail.com>
2022-12-14 23:28:21 -0300

    Service

---
+++ Blazor and ASP.NET/Loja/Service/CategoriaService.cs
@@ -0,0 +1,55 @@
+﻿using Core;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Service
+{
+    public class CategoriaService : ICategoria
+    {
+        private readonly LojaContext _dbContext;
+
+        public CategoriaService(LojaContext lojaContext)
+        {
+            _dbContext = lojaContext;
+        }
+        public async Task<Categoria> Create(Categoria categoria)
+        {
+            await _dbContext.Categoria.AddAsync(categoria);
+            await _dbContext.SaveChangesAsync();
+            return categoria;
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            Categoria categoria = await Get(id);
+            _dbContext.Categoria.Remove(categoria);
+            await _dbContext.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<Categoria> Edit(Categoria categoria, int id)
+        {
+            Categoria getedCategoria = await Get(id);
+            getedCategoria = categoria;
+
+            _dbContext.Categoria.Update(getedCategoria);
+            await _dbContext.SaveChangesAsync();
+            return getedCategoria;
+        }
+
+        public async Task<Categoria> Get(int id)
+        {
+            Categoria? categoria = await _dbContext.Categoria.AsNoTracking().FirstOrDefaultAsync(x => x.Id.Equals(id));
+            if (categoria == null)
+                throw new Exception($"Não existe categoria correspondente ao ID: {id}");
+
+            return categoria;
+        }
+
+        public async Task<IEnumerable<Categoria>> GetAll()
+        {
+            return await _dbContext.Categoria.AsNoTracking().ToListAsync();
+        }
+    }
+}
---
+++ Blazor and ASP.NET/Loja/Service/ProdutoService.cs
@@ -0,0 +1,55 @@
+﻿using Core;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Service
+{
+    public class ProdutoService : IProduto
+    {
+        private readonly LojaContext _dbContext;
+        public ProdutoService(LojaContext lojaContext)
+        {
+            _dbContext = lojaContext;
+        }
+
+        public async Task<Produto> Create(Produto produto)
+        {
+            await _dbContext.Produto.AddAsync(produto);
+            await _dbContext.SaveChangesAsync();
+            return produto;
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            Produto produto = await Get(id);
+            _dbContext.Produto.Remove(produto);
+            await _dbContext.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<Produto> Edit(Produto produto, int id)
+        {
+            Produto getedProduto = await Get(id);
+            getedProduto = produto;
+
+            _dbContext.Produto.Update(getedProduto);
+            await _dbContext.SaveChangesAsync();
+            return getedProduto;
+        }
+
+        public async Task<Produto> Get(int id)
+        {
+            Produto? produto = await _dbContext.Produto.AsNoTracking().FirstOrDefaultAsync(x => x.Id.Equals(id));
+            if (produto == null)
+                throw new Exception($"Não existe produto correspondente ao ID: {id}");
+
+            return produto;
+        }
+
+        public async Task<IEnumerable<Produto>> GetAll()
+        {
+            return await _dbContext.Produto.AsNoTracking().ToListAsync();
+        }
+    }
+}
