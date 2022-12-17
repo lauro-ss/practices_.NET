8071a6a6a646d1dc961129b968503285df9559b8
Lauro Santana Silva <bestlaurobr@gmail.com>
2022-12-17 11:47:26 -0300

    Add validation attributes

--- Blazor and ASP.NET/Loja/Core/Categoria.cs
+++ Blazor and ASP.NET/Loja/Core/Categoria.cs
@@ -8,11 +8,11 @@ namespace Core
         public int Id { get; set; }
         [Required(ErrorMessage = "O campo Nome é obrigatório.")]
         [StringLength(150, ErrorMessage = "O Nome só pode conter até 150 caracteres.")]
-        public string Nome { get; set; }
+        public string Nome { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "É obrigatório ter ao menos uma Subcategoria.")]
         [StringLength(150, ErrorMessage = "A Subcategoria só pode conter até 150 caracteres.")]
-        public string Subcategoria_1 { get; set; }
+        public string Subcategoria_1 { get; set; } = string.Empty;
 
         [StringLength(150, ErrorMessage = "A Subcategoria só pode conter até 150 caracteres.")]
         public string? Subcategoria_2 { get; set; }
--- Blazor and ASP.NET/Loja/Core/Produto.cs
+++ Blazor and ASP.NET/Loja/Core/Produto.cs
@@ -1,22 +1,24 @@
 ﻿using Core.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace Core
 {
     public class Produto
     {
         public int Id { get; set; }
-
-        public string Nome { get; set; }
-
-        public decimal Valor { get; set; }
-
+        [Required(ErrorMessage = "O campo Nome é obrigatório.")]
+        [StringLength(150, ErrorMessage = "O Nome só pode conter até 150 caracteres.")]
+        public string Nome { get; set; } = string.Empty;
+        [Required(ErrorMessage = "O campo Valor é obrigatório.")]
+        public decimal Valor { get; set; } = 0.0M;
+        [Required(ErrorMessage = "O campo Status é obrigatório.")]
         public StatusProduto Status { get; set; }
-
+        [Required(ErrorMessage = "O campo Quantidade é obrigatório.")]
         public int Quantidade { get; set; }
-
+        [Required(ErrorMessage = "O campo Categoria é obrigatório.")]
         public int IdCategoria { get; set; }
-
-        public string Subcategoria { get; set; }
+        [Required(ErrorMessage = "O campo Subcategoria é obrigatório.")]
+        public string Subcategoria { get; set; } = string.Empty;
 
         public Categoria? Categoria { get; set; }
     }
