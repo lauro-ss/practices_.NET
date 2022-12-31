3ec92d5ce2c1dd9bcd956492d2dec77c8066e784
Lauro Santana Silva <bestlaurobr@gmail.com>
2022-12-31 19:06:46 -0300

    Adicionado validações e expressões regulares a Produto

--- Blazor and ASP.NET/Loja/Core/Produto.cs
+++ Blazor and ASP.NET/Loja/Core/Produto.cs
@@ -10,12 +10,19 @@ namespace Core
         [StringLength(150, ErrorMessage = "O Nome só pode conter até 150 caracteres.")]
         public string Nome { get; set; } = string.Empty;
         [Required(ErrorMessage = "O campo Valor é obrigatório.")]
+        [RegularExpression("^[0-9]{1,10}(\\,[0-9]{0,2})*", ErrorMessage = "Valor inválido")]
         public decimal Valor { get; set; } = 0.0M;
+
         [Required(ErrorMessage = "O campo Status é obrigatório.")]
+        [Range(1, 100, ErrorMessage = "O campo Status é obrigatório.")]
         public StatusProduto Status { get; set; }
+
         [Required(ErrorMessage = "O campo Quantidade é obrigatório.")]
+        [RegularExpression("[0-9]{1,10}", ErrorMessage = "Quantidade inválida")]
         public int Quantidade { get; set; }
+
         [Required(ErrorMessage = "O campo Categoria é obrigatório.")]
+        [RegularExpression("[1-9]{1,10}", ErrorMessage = "O campo Categoria é obrigatório.")]
         public int IdCategoria { get; set; }
         [Required(ErrorMessage = "O campo Subcategoria é obrigatório.")]
         public string Subcategoria { get; set; } = string.Empty;
