a2d92e2523a39735a28a5ab3c81e545a01dad40f
Lauro Santana Silva <bestlaurobr@gmail.com>
2023-01-02 15:44:14 -0300

    Add ProdutoDTO

---
+++ Blazor and ASP.NET/Loja/Core/DTOs/ProdutoDTO.cs
@@ -0,0 +1,17 @@
+﻿using Core.Enums;
+
+#nullable disable
+
+namespace Core.DTOs
+{
+    public class ProdutoDTO
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Valor { get; set; }
+        public StatusProduto Status { get; set; }
+        public int Quantidade { get; set; }
+        public string Categoria { get; set; }
+        public string Subcategoria { get; set; }
+    }
+}
--- Blazor and ASP.NET/Loja/Core/Interfaces/IProduto.cs
+++ Blazor and ASP.NET/Loja/Core/Interfaces/IProduto.cs
@@ -1,9 +1,12 @@
-﻿namespace Core.Interfaces
+﻿using Core.DTOs;
+
+namespace Core.Interfaces
 {
     public interface IProduto
     {
         Task<Produto> Get(int id);
         Task<IEnumerable<Produto>> GetAll();
+        Task<IEnumerable<ProdutoDTO>> GetAllDTO();
         Task<Produto> Create(Produto produto);
         Task<Produto> Edit(Produto produto, int id);
         Task<bool> Delete(int id);
